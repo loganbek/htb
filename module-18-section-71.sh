b1d532f1fc87153482d8475841cbd3af8aedda6d
Logan Bek <loganbek@gmail.com>
2024-05-13 12:32:02 -0700

    Create module-18-section-71.sh

---
+++ module-18-section-71.sh
@@ -0,0 +1,10 @@
+# https://academy.hackthebox.com/module/18/section/71
+
+# Which option needs to be set to create a home directory for a new user using "useradd" command?
+-m
+
+# Which option needs to be set to lock a user account using the "usermod" command? (long version of the option)
+--lock
+
+# Which option needs to be set to execute a command as a different user using the "su" command? (long version of the option)
+--comand
