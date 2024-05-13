4e0951c14149ce41d8986ae8ecf745e4b1af0406
Logan Bek <loganbek@gmail.com>
2024-05-13 10:57:13 -0700

    Create module-18-section-81.sh

---
+++ module-18-section-81.sh
@@ -0,0 +1,10 @@
+# https://academy.hackthebox.com/module/18/section/81
+
+# What is the name of the config file that has been created after 2020-03-03 and is smaller than 28k but larger than 25k?
+find / -type f -name "*.config" -size +25k -size -28k -newermt 2020-03-03
+
+#  How many files exist on the system that have the ".bak" extension?
+find / -type f -name "*.bak"
+
+# Submit the full path of the "xxd" binary.
+which xxd
