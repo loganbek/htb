869a319cae56f3bf7ae4064978f3ebd788a43538
Logan Bek <loganbek@gmail.com>
2024-05-13 13:14:25 -0700

    Update module-77-section-726.sh
    finish last

--- module-77-section-726.sh
+++ module-77-section-726.sh
@@ -7,3 +7,7 @@ nmap -sV -p 8080 <TARGET_IP>
 nmap -p- -sV <TARGET_IP>
 
 # List the SMB shares available on the target host. Connect to the available share as the bob user. Once connected, access the folder called 'flag' and submit the contents of the flag.txt file.
+smbclient //target_ip/share_name -U bob
+cd flag
+get flag.txt
+more flag.txt
