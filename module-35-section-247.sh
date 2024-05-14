4b0f3c4024506e2312eb09839b5cd057fd967904
Logan Bek <loganbek@gmail.com>
2024-05-13 19:00:10 -0700

    Create module-35-section-247.sh

---
+++ module-35-section-247.sh
@@ -0,0 +1,3 @@
+# https://academy.hackthebox.com/module/35/section/247
+
+# The exercise above seems to be broken, as it returns incorrect results. Use the browser devtools to see what is the request it is sending when we search, and use cURL to search for 'flag' and obtain the flag.
