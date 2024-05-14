249cee8c20ddf982d4f9593bf64671ab07c14399
Logan Bek <loganbek@gmail.com>
2024-05-13 18:47:01 -0700

    Create module-157-section-1530.sh

---
+++ module-157-section-1530.sh
@@ -0,0 +1,3 @@
+# https://academy.hackthebox.com/module/157/section/1530
+
+# Download the above file and double click on it to unzip it. The extracted folder may appear empty, but in reality it has a hidden file with the flag. Can you find the flag?
