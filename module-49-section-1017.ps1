631c57ac432f7c86ab56ffc12b5c49f39447d6f6
Logan Bek <loganbek@gmail.com>
2024-05-13 18:26:28 -0700

    Create module-49-section-1017.ps1

---
+++ module-49-section-1017.ps1
@@ -0,0 +1,5 @@
+# https://academy.hackthebox.com/module/49/section/1017
+
+# What is the name of the utility that can be used to view logs made by a Windows system? (Format: 2 words, 1 space, not case sensitive)
+
+# What is the full directory path to the Company Data share we created?
