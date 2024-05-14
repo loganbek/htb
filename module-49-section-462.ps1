b6c574b5a97a6b6047c6dff2882c16f1853e8976
Logan Bek <loganbek@gmail.com>
2024-05-13 18:31:55 -0700

    Create module-49-section-462.ps1

---
+++ module-49-section-462.ps1
@@ -0,0 +1,5 @@
+# https://academy.hackthebox.com/module/49/section/462
+
+# Find the SID of the bob.smith user.
+
+# What 3rd party security application is disabled at startup for the current user? (The answer is case sensitive).
