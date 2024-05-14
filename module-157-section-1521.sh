0b9fa75d5b302bf8112360c67bd1175fc627a3af
Logan Bek <loganbek@gmail.com>
2024-05-13 18:50:23 -0700

    Create module-157-section-1521.sh

---
+++ module-157-section-1521.sh
@@ -0,0 +1,4 @@
+# https://academy.hackthebox.com/module/157/section/1521
+
+# If a file has a permission set of "rw-rw-rw-" applied, what would that equal in Octal format? (number only)
+6 + 6 + 6 = 666
