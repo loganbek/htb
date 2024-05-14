7215a5623a3ba6da53658961d05717b3dad98d35
Logan Bek <loganbek@gmail.com>
2024-05-13 18:44:50 -0700

    Create module-157-section-1520.sh

---
+++ module-157-section-1520.sh
@@ -0,0 +1,7 @@
+# https://academy.hackthebox.com/module/157/section/1520
+
+# What BSD derivative is the basis of the macOS operating system?
+Darwin
+
+# What provides the desktop experience and file management capabilities within macOS?
+Finder
