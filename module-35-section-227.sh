75c6c30002870ad74172228dbf73073823a20d25
Logan Bek <loganbek@gmail.com>
2024-05-13 19:02:08 -0700

    Create module-35-section-227.sh

---
+++ module-35-section-227.sh
@@ -0,0 +1,3 @@
+# https://academy.hackthebox.com/module/35/section/227
+
+# First, try to update any city's name to be 'flag'. Then, delete any city. Once done, search for a city named 'flag' to get the flag.
