a7df75528ce3bebf1963e2ac2df3732068445e8e
Logan Bek <loganbek@gmail.com>
2024-05-13 19:01:03 -0700

    Create module-35-section-224.sh

---
+++ module-35-section-224.sh
@@ -0,0 +1,3 @@
+# https://academy.hackthebox.com/module/35/section/224
+
+# Obtain a session cookie through a valid login, and then use the cookie with cURL to search for the flag through a JSON POST request to '/search.php'
