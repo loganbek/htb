126a5d423fbea56939cb1397b3bb7f66088483d2
Logan Bek <loganbek@gmail.com>
2024-05-13 18:59:00 -0700

    Create module-35-223.sh

---
+++ module-35-223.sh
@@ -0,0 +1,3 @@
+# https://academy.hackthebox.com/module/35/section/223
+
+# The server above loads the flag after the page is loaded. Use the Network tab in the browser devtools to see what requests are made by the page, and find the request to the flag.
