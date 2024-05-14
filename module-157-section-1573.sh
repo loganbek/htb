77427336567daff785af09ed5149015c8ee4c9a0
Logan Bek <loganbek@gmail.com>
2024-05-13 18:53:37 -0700

    Create module-157-section-1573.sh

---
+++ module-157-section-1573.sh
@@ -0,0 +1,4 @@
+# https://academy.hackthebox.com/module/157/section/1523
+
+# Read the zsh configuration shown in the section above to find what command is mapped to 'll'. Submit the command as the answer.
+
