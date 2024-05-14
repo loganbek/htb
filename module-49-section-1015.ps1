7fc420996e48d531cd5c25355a5382b34eedf9a2
Logan Bek <loganbek@gmail.com>
2024-05-13 18:33:06 -0700

    Create module-49-section-1015.ps1

---
+++ module-49-section-1015.ps1
@@ -0,0 +1,7 @@
+# https://academy.hackthebox.com/module/49/section/1015
+
+# What is the name of the group that is present in the Company Data Share Permissions ACL by default?
+
+# What is the name of the tab that allows you to configure NTFS permissions?
+
+# What is the name of the service associated with Windows Update?
