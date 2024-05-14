f26c42b7581c25fa53d89cdadff39f2ec97ad358
Logan Bek <loganbek@gmail.com>
2024-05-13 18:56:55 -0700

    Create module-35-section-219.sh

---
+++ module-35-section-219.sh
@@ -0,0 +1,3 @@
+# https://academy.hackthebox.com/module/35/section/219
+
+# To get the flag, start the above exercise, then use cURL to download the file returned by '/download.php' in the server shown above.
