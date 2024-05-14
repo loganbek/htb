9ccf9cbb18b1fce902e912b8c8d65f2677b00cdb
Logan Bek <loganbek@gmail.com>
2024-05-13 18:27:26 -0700

    Create module-49-section-457.ps1

---
+++ module-49-section-457.ps1
@@ -0,0 +1,3 @@
+# https://academy.hackthebox.com/module/49/section/457
+
+# Identify one of the non-standard update services running on the host. Submit the full name of the service executable (not the DisplayName) as your answer.
