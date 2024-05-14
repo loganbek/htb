a67fccfd5008d7ebf2b3f5aaf498f59d19deb60d
Logan Bek <loganbek@gmail.com>
2024-05-13 18:23:12 -0700

    Create module-42-section-454.ps1

---
+++ module-42-section-454.ps1
@@ -0,0 +1,5 @@
+# https://academy.hackthebox.com/module/49/section/454
+
+# What is the Build Number of the target workstation?
+
+# Which Windows NT version is installed on the workstation? (i.e. Windows X - case sensitive)
