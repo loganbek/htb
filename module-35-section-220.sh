2b99a6e535e569f9312e41d6933640437dff1011
Logan Bek <loganbek@gmail.com>
2024-05-13 18:58:20 -0700

    Create module-35-section-220.sh

---
+++ module-35-section-220.sh
@@ -0,0 +1,5 @@
+# https://academy.hackthebox.com/module/35/section/220
+
+# What is the HTTP method used while intercepting the request? (case-sensitive)
+
+# Send a GET request to the above server, and read the response headers to find the version of Apache running on the server, then submit it as the answer. (answer format: X.Y.ZZ)
