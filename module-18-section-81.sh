f0fbc6108f85b6df6f595ea5e9b6bc167b6e766d
loganbek <loganbek@gmail.com>
2024-05-18 20:19:17 +0000

    find file and directories module correct commands

--- module-18-section-81.sh
+++ module-18-section-81.sh
@@ -1,12 +1,11 @@
 # https://academy.hackthebox.com/module/18/section/81
 
 # What is the name of the config file that has been created after 2020-03-03 and is smaller than 28k but larger than 25k?
-find / -type f -name "*.config" -size +25k -size -28k -newermt 2020-03-03
-## OR
-find / -type f -name "*.config" -size +25k -size -28k -newermt 2030-03-03 | grep -v "Permission denied:"
+
+find / -type f -name *.conf -size +25k -size -28k -newermt 2020-03-03 -exec basename {} \; 2>/dev/null
 
 #  How many files exist on the system that have the ".bak" extension?
-find / -type f -name "*.bak"
+find / -type f -name "*.bak" | grep -v "Permission denied:" | wc -l
 
 # Submit the full path of the "xxd" binary.
 which xxd
